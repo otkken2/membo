cb120d4eab66f8f3ff1ef60f3bfd8820bac7eba9
otkken2 <lovelovezamakko2@gmail.com>
2020-01-28 22:51:19 +0900

    []

--- membo/settings.py
+++ membo/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from configs import twitter
+from project.configs import twitter
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
