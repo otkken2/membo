8db51f1ca751dbbe1699a6a2233d73fc66f248d8
otkken2 <lovelovezamakko2@gmail.com>
2020-03-13 10:35:01 +0900

    re fixed settings.py about installed apps

--- membo/settings.py
+++ membo/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'widget_tweaks',
     'social_django',
     'debug_toolbar',
