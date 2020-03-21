344a8eab6e75fc766050f1ba4dbe77ccddf84c9e
otkken2 <lovelovezamakko2@gmail.com>
2020-03-22 01:54:18 +0900

    fixed asgilpy

--- membo/asgi.py
+++ membo/asgi.py
@@ -10,6 +10,6 @@ import channels.asgi
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "membo.settings")
-# django.setup()
-# application = get_default_application()
-channel_layer = channels.asgi.get_channel_layer()
+django.setup()
+application = get_default_application()
+# channel_layer = channels.asgi.get_channel_layer()
