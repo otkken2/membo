978af830266722317f3d5d8950b9b3aa5f9ef329
otkken2 <lovelovezamakko2@gmail.com>
2020-03-22 13:54:59 +0900

    fixed asgi.py

--- membo/asgi.py
+++ membo/asgi.py
@@ -6,8 +6,13 @@ defined in the ASGI_APPLICATION setting.
 import os
 import django
 from channels.routing import get_default_application
+# import channels.asgi 
+from channels.layers import get_channel_layer
+
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "membo.settings")
 django.setup()
 application = get_default_application()
+# channel_layer = channels.asgi.get_channel_layer()
+channel_layer = get_channel_layer()
