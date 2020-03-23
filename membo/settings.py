f4a16d2dc8f6caedb0055da7f73d5ffbbdbe4c67
otkken2 <lovelovezamakko2@gmail.com>
2020-03-23 12:59:54 +0900

    fixed settings.py (asgi_redis ⇨ channels_redis)

--- membo/settings.py
+++ membo/settings.py
@@ -180,22 +180,22 @@ ASGI_APPLICATION = 'membo.routing.application'
 #         },
 #     },
 # }
-CHANNEL_LAYERS = {
-    'default':{
-        'BACKEND': 'asgi_redis.RedisChannelLayer',
-        'CONFIG':{
-            "hosts":[os.environ.get('REDIS_URL','redis://localhost:6379')],
-        },
-    },
-}
 # CHANNEL_LAYERS = {
 #     'default':{
-#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#         'BACKEND': 'asgi_redis.RedisChannelLayer',
 #         'CONFIG':{
-#             "hosts":[os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+#             "hosts":[os.environ.get('REDIS_URL','redis://localhost:6379')],
 #         },
 #     },
 # }
+CHANNEL_LAYERS = {
+    'default':{
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG':{
+            "hosts":[os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+        },
+    },
+}
 
 INTERNAL_IPS = [
     '127.0.0.1',
